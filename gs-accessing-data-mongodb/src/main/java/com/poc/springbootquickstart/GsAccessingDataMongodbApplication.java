c94c4c593e4da6303ded5db2c176eb82f4dd8e8d
Srikanth Padakanti <srikanth29.9@gmail.com>
2017-06-07 10:58:05 -0400

    Second Commit

--- gs-accessing-data-mongodb/src/main/java/com/poc/springbootquickstart/GsAccessingDataMongodbApplication.java
+++ gs-accessing-data-mongodb/src/main/java/com/poc/springbootquickstart/GsAccessingDataMongodbApplication.java
@@ -2,10 +2,13 @@ package com.poc.springbootquickstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class GsAccessingDataMongodbApplication {
 
+	//This is my second commit
+	//This will be shown after my commit 
 	public static void main(String[] args) {
 		SpringApplication.run(GsAccessingDataMongodbApplication.class, args);
 	}
